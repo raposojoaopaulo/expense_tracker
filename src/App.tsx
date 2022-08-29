c675474410afcc0ace7e2b2199a0f6c722635f07
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-08-29 10:07:12 -0300

    initial adjustments and initial styles from components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Comp from './App.styles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Comp.Container>
+      <Comp.Header>
+        <Comp.HeaderText>Expense Tracker</Comp.HeaderText>
+      </Comp.Header>
+      <Comp.Body>
+        <h3>Hello</h3>
+      </Comp.Body>
+    </Comp.Container>
   );
 }
 
