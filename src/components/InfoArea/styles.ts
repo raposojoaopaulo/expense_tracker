cb16d4b8a3d351e0f8b2c99c3fbf9c29f9990463
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-08-30 17:27:56 -0300

    created form for add new item

--- src/components/InfoArea/styles.ts
+++ src/components/InfoArea/styles.ts
@@ -22,7 +22,12 @@ export const MonthArrow = styled.div`
     width: 40px;
     text-align: center;
     font-size: 25px;
+    border-radius: 5px;
     cursor: pointer;
+    :active {
+        box-shadow: inset 4px 4px 12px #c5c5c5,
+                    inset -4px -4px 12px #ffffff;
+       }
 `;
 
 export const MonthTitle = styled.div`
