b1836ae0dcd4bc5b0a1c91dea9d1eabcd08c37c6
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2022-08-30 11:53:03 -0300

    Added summary information display

---
+++ src/components/ResumeItem/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Info, Title } from './styles'
+
+type Props = {
+  title: string;
+  value: number;
+  color?: string;
+}
+
+export const ResumeItem = ({ title, value, color }: Props) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Info color={color}>
+        R${value}
+      </Info>
+    </Container>
+  );
+}
